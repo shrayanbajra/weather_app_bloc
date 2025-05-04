fd712716abef537d268f1c3b9a9562db34eec9c2
shrayan <shrayanbajracharya@gmail.com>
2025-05-04 14:55:43 +0545

    Added code for starter

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app_bloc/presentation/screens/weather_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Weather App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const WeatherScreen(),
+    );
+  }
+}
